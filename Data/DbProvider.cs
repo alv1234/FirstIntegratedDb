73a56c35afd6becfa3df1b609f5a230bdf31f7ba
Alessandro <volcich.alessandro@hotmail.com>
2018-06-01 18:59:21 +0200

    Add project files.

---
+++ Data/DbProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DbProvider
+    {
+
+        //DbProvider-ToDo
+        //TODO Initialize DB-Model Object 
+        ProjectDbEntities dbmodel = new ProjectDbEntities();
+        //TODO Write Linq-Queries
+
+
+        //TODO LinqQueryAll
+        public List<Person> QueryAll()
+        {
+            var query = (from b in dbmodel.People
+                         select new
+                         {
+                             Id = b.Id,
+                             Name = b.Name,
+                         }).ToList().Select(x => new Person
+                         {
+                             Id = x.Id,
+                             Name = x.Name
+                         });
+            return query.ToList();
+        }
+
+
+        //TODO LinqInsert
+        public int Add(string b)
+        {
+            dbmodel.People.Add(new Person()
+            {
+                Name = b
+            });
+
+            return dbmodel.SaveChanges();
+        }
+    }
+}
---
+++ FirstIntegratedDb/Program.cs
@@ -0,0 +1,41 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstIntegratedDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbProvider db = new DbProvider();
+            Console.WriteLine("Hello");
+            int i = 1;
+            while (i != 0)
+            {
+                Console.WriteLine("\nPlease choose the following...\n\t1 - query all \n\t2 - insert data\n\t0 - exit");
+                i = Int32.Parse(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        foreach (var item in db.QueryAll())
+                        {
+                            Console.WriteLine(String.Format("{0}-{1}", item.Id, item.Name));
+                        }
+                        break;
+                    case 2:
+                        string temp = "NewName" + new Random().Next(1000);
+                        db.Add(temp);
+                        Console.WriteLine(String.Format("{0} was added to list", temp));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+    }
+}
